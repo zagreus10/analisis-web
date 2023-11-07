40ccf289be8189ef0c8a9488853ece956ba4690e
Kenneth Martinez <145396445+RedKnight023@users.noreply.github.com>
2023-11-06 18:51:13 -0600

    Update analisis.py
    Adaptacion para funcionar API y codigo general
    Revisar cual sera por defecto y como

--- analisis.py
+++ analisis.py
@@ -1,19 +1,28 @@
 import argparse
-from funciones import busquedaCorreos, eliminarArchivosPrevios
+from funciones import busquedaCorreos, eliminarArchivosPrevios, leer_k, virus_api
 
-if __name__ == '__main__': 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-url", dest="url", help="Ingrese una url de la web a analizar",required=True)
-
+if __name__ == '__main__':
+    #Argumento por defecto
+    eliminarArchivosPrevios()
+    parser = argparse.ArgumentParser(description="Codigo con herramientas de ciberseguridad")
+    parser.add_argument("-url", dest="url", help="Ingrese una url de la web a analizar")
+    parser.add_argument("-sct", metavar="ScannTarget", dest="ScnTarget", help="Nombre del archivo a analizar")
     params = parser.parse_args()
 
-    if len(params.url) > 0:
-        """ 
-        En caso de que se tengan archivos previos se elimina lo pasado y empieza a ejecutar el codigo
-        """
-        eliminarArchivosPrevios()
-        busquedaCorreos(params.url)
-        
-    else:
-        ("La url es requerida")
+    if params.ScnTarget:
+        file = params.ScnTarget
+        key = leer_k()
+        if key:
+            info = virus_api(file, key)
+            with open("report_file.txt", "w") as f:
+                f.write(info)
+
+    if params.url:
+            """ 
+            En caso de que se tengan archivos previos se elimina lo pasado y empieza a ejecutar el codigo
+            """
+        if len(params.url) > 0:
+            busquedaCorreos(params.url)
+        else:
+            print("La URL es requerida")
+            
